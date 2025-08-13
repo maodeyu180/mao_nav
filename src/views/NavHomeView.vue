ad30a68ffc1eae1eb8c742c00e836c2edb7e2aa4
dashugzs <87010009+dashugzs@users.noreply.github.com>
2025-08-13 21:35:24 +0800

    Update NavHomeView.vue

--- src/views/NavHomeView.vue
+++ src/views/NavHomeView.vue
@@ -281,7 +281,7 @@ const searchEngines = {
     icon: jingdongLogo,
     placeholder: '（京东）输入商品的名字关键词'
   },
-  jingdong: {
+  jiexi: {
     url: 'https://z1.m1907.top/?jx=',
     icon: jiexiLogo,
     placeholder: '（视频解析输入剧名或链接）失效左下角版权信息处联系我'
