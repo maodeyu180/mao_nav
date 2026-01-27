2c1e2acd450899b7f666056f1e62b40b7ace81ae
DD-lgtm66 <RimaGuignarduls14@gmail.com>
2026-01-27 21:10:08 +0800

    chore: 更新导航数据 - 2026/1/27 21:10:04

--- src/mock/mock_data.js
+++ src/mock/mock_data.js
@@ -481,6 +481,6 @@ export const mockData = {
       ]
     }
   ],
-  "title": "猫猫导航🐱",
-  "_fileSha": "b03d0407ec06ac7f254abf6645ac0425a8e74a92"
+  "title": "彩虹导航🌈",
+  "_fileSha": "ea3f00cba99e7d9e1d5cb7b816f6a7836b8c233e"
 }
